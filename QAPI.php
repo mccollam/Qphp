169139e95f47d19c610cd2590296a60631eac56a
Ronald McCollam <mccollam@gmail.com>
2015-03-07 17:35:19 -0500

    Added a few more API calls (notably querying bulbs)

--- QAPI.php
+++ QAPI.php
@@ -5,6 +5,10 @@ class Q
 	public $ip = '127.0.0.1';
 	public $port = 11600;
 	
+	private $bufsize = 2048;	// max accepted response (in bytes)
+	
+	/***** Public methods *****/
+	
 	public function Q($ip='127.0.0.1', $port=11600)
 	{
 		$this->ip = $ip;
@@ -31,7 +35,46 @@ class Q
 		$this->q_send_command($this->build_json_light_control($bulbs, 0, 0, 0, 0, 0, 0, 8)); 
 		return true;
 	}
+	
+	public function list_bulbs()
+	{
+		// List the bulbs known by a Q station
+		$bulbs = $this->q_send_command_expect_response("{ 'cmd':'light_list' }");
+		return json_decode($bulbs, true)['led'];
+	}
+	
+	public function set_bulb_name($bulb, $title='lightX')
+	{
+		$json = "{ 'cmd':'set_title', 'sn':'" . $bulb . "','title':'" . $title . "' }";
+		$this->q_send_command($json);
+	}
+	
+	public function save_lights()
+	{
+		// FIXME: Unimplemented (Not sure what this does yet!)
+	}
+	
+	public function music_sync($bulb)
+	{
+		// FIXME: It doesn't look like there's a way to *unset* this!
+		$json = "{ 'cmd':'set_musicled', 'sn':'" . $bulb . "' }";
+		$this->q_send_command($json);
+	}
+	
+	public function music_sync_group($group)
+	{
+		// FIXME: Unimplemented (How does this differ from normal music sync?)
+	}
+	
+	public function delete($bulb)
+	{
+		// Delete a bulb from the Q station
+		// FIXME: Is there a way to re-add them?  (This is untested until I find that out!)
+		$json = "{ 'cmd':'del_device', 'sn':'" . $bulb . "' }";
+		$this->q_send_command($json);
+	}
 
+	/***** Private methods *****/
 
 	private function q_send_command($json)
 	{
@@ -41,6 +84,21 @@ class Q
 		socket_sendto($sock, $json, $len, 0, $this->ip, $this->port);
 		
 		socket_close($sock); 
+	}
+	
+	private function q_send_command_expect_response($json)
+	{
+		$sock = socket_create(AF_INET, SOCK_DGRAM, SOL_UDP);
+		socket_set_option($sock, SOL_SOCKET, SO_RCVTIMEO, array("sec"=>1, "usec"=>0));
+		$len = strlen($json);
+		$response = '';
+		
+		socket_sendto($sock, $json, $len, 0, $this->ip, $this->port);
+		socket_recvfrom($sock, $response, $this->bufsize, 0, $this->ip, $this->port);
+		
+		socket_close($sock);
+		
+		return $response;
 	}
 	
 	private function build_json_light_control($bulbs, $red, $green, $blue, $bright, $iswitch, $matchValue, $effect)
